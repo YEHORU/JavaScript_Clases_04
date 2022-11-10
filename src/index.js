be67fd37ad9fc913303df8ccb88e516a745d4ced
yehoru <niko.xero45x@gmail.com>
2022-11-10 16:51:27 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 
-import { saludar } from "./js/componentes";
+// import { todoList } from "./classes/todo-list.class";
+// import { Todo } from "./classes/todo.class";
+import { Todo, TodoList } from "./classes";
+import { crearTodoHtml } from "./js/componentes";
 import "./style.css";
 
-const nombre = 'Pete';
 
-saludar(nombre);
+export const todoList = new TodoList();
+
+// todoList.todos.forEach(todo => {
+//     crearTodoHtml(todo);
+// });
+
+todoList.todos.forEach( crearTodoHtml );
+
+
+//const todoList = new TodoList();
+// const tarea = new Todo('Apredender JavaScript');
+
+
+
+// todoList.nuevoTodo(tarea);
+
+// tarea.completado = true;
+
+// console.log(todoList);
+
+// crearTodoHtml( tarea );
+
+// localStorage.setItem('mi-key', 'ABC123');
+// sessionStorage.setItem('mi-key', 'ABC123');
+
+// setTimeout(() => {
+//     localStorage.removeItem('')
+// }, 1500 );
